8d09de588ef0582da751ad2a77c69860e92634ae
nethmiTK <nethmitk33@gmail.com>
2025-03-04 11:11:51 +0530

    Initial commit

---
+++ src/main/java/com/example/Travel_Trace/TravelTraceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Travel_Trace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelTraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelTraceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Travel_Trace/TravelTraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Travel_Trace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelTraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
